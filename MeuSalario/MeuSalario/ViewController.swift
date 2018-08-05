1b3c015cb0c80f1c7fbb9108478710d270f0529b
dmandarino <Douglas Mandarino>
2018-08-05 00:17:44 -0300

    First Commit

---
+++ MeuSalario/MeuSalario/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeuSalario
+//
+//  Created by Douglas Mandarino on 05/08/18.
+//  Copyright © 2018 Douglas Mandarino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
